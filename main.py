c00ff17065817558991898d7d997cb302e221d44
gmthubmadharchod <sonusinghtomar778@gmail.com>
2026-03-12 15:32:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,30 +6,44 @@ from logger import LOGGER
 from modules.retasks import recover_incomplete_batches
 from modules.scheduler import start_daily_schedulers
 from flask import Flask
+import threading
+import os
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Flask App running!"
+    return "Bot is running!"
+
+def run_flask():
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
+
+bot = Client(
+    "Bot",
+    bot_token=Config.BOT_TOKEN,
+    api_id=Config.API_ID,
+    api_hash=Config.API_HASH,
+    sleep_threshold=30,
+    plugins=dict(root="plugins"),
+    workers=100
+)
+
+async def main():
+    await bot.start()
+    bot_info = await bot.get_me()
+    LOGGER.info(f"<--- @{bot_info.username} Started --->")
+
+    asyncio.create_task(recover_incomplete_batches(bot))
+    asyncio.create_task(start_daily_schedulers(bot))
+    LOGGER.info("Daily update schedulers started")
+
+    await idle()
+
 if __name__ == "__main__":
-    bot = Client(
-        "Bot",
-        bot_token=Config.BOT_TOKEN,
-        api_id=Config.API_ID,
-        api_hash=Config.API_HASH,
-        sleep_threshold=30,
-        plugins=dict(root="plugins"),
-        workers=1000,
-    )
     
-    async def main():
-        await bot.start()
-        bot_info = await bot.get_me()
-        LOGGER.info(f"<--- @{bot_info.username} Started --->")
-        asyncio.create_task(recover_incomplete_batches(bot))
-        asyncio.create_task(start_daily_schedulers(bot))
-        LOGGER.info("Daily update schedulers started")
-        await idle()
-    
+    # Flask server thread
+    threading.Thread(target=run_flask).start()
+
     asyncio.get_event_loop().run_until_complete(main())
+
     LOGGER.info("<--- Bot Stopped --->")
